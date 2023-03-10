5cee6e90512bf9055c35fa613facd3360ef54254
hostman2312 <123991788+ghost2539@users.noreply.github.com>
2023-03-09 21:43:35 -0300

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -3,12 +3,13 @@ export const PORT = 443;
 export const ZONE = "BomberGameZone";
 export const SALT_KEY = "f17e4e44f7bbc229fdc2d2f55728abba";
 export const DATE_OFFSET = 62135596800000;
-export const VERSION_CODE = 23030711;
+export const VERSION_CODE = 23030805;
 export const SERVERS = ["na", "sea", "sa"];
 export const LC = "";
 
 //export const WEB3_RPC = "https://polygon-rpc.com";
-export const WEB3_RPC = "https://rpc-mainnet.maticvigil.com";
+//export const WEB3_RPC = "https://rpc-mainnet.maticvigil.com";
+export const WEB3_RPC = "https://polygon-rpc.com";
 
 export const BLOCK_REWARD_TYPE_BCOIN_POLYGON = 1;
 export const CONTRACT_APPROVE_CLAIM =
