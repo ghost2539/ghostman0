9f2744c2959c7be1ddfaaf4461f7e23315c8485c
hostman2312 <123991788+ghost2539@users.noreply.github.com>
2023-03-14 21:12:14 -0300

    Update versão

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const PORT = 443;
 export const ZONE = "BomberGameZone";
 export const SALT_KEY = "f17e4e44f7bbc229fdc2d2f55728abba";
 export const DATE_OFFSET = 62135596800000;
-export const VERSION_CODE = 23031017;
+export const VERSION_CODE = 23031404;
 export const SERVERS = ["na", "sea", "sa"];
 export const LC = "";
 
