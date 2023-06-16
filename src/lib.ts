5674deddcb7f95e70a19e47a86ea794ff86b4472
hostman2312 <123991788+ghost2539@users.noreply.github.com>
2023-06-16 14:50:09 -0300

    Update lib.ts

--- src/lib.ts
+++ src/lib.ts
@@ -203,7 +203,7 @@ export const getChatId = (ctx: any) => {
 
 export const getGasPolygon = async () => {
     const { standard } = await got
-            .get(`https://gasstation.polygon.technology/v2/?data=${Date.now()}`, {`, {
+            .get(`https://gasstation.polygon.technology/v2/?data=${Date.now()}`, {`
             headers: { "cache-control": "no-cache" },
         })
         .json<{
